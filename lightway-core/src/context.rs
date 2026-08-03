3a0ee0d353abe5dc566b5d67ebb611b582a5f5f0
ben.hung@kape.com <ben.hung@kape.com>
2026-08-03 15:29:16 +0800

    core: drop DTLS1.2 support for Datagram traffic

--- lightway-core/src/context.rs
+++ lightway-core/src/context.rs
@@ -447,20 +447,12 @@ impl<AppState> ServerContextBuilder<AppState> {
     ) -> ContextBuilderResult<Self> {
         let protocol = match connection_type {
             ConnectionType::Stream => crate::tls::Method::TlsServerV1_3,
-            // Since "DtlsServer" refers to both 1.2 and 1.3,
-            // and 1.2 is not yet implemented on boringssl side, we will feature gate this to
-            // prevent breaking change on wolfssl side for this commit.
-            #[cfg(wolfssl)]
-            ConnectionType::Datagram => crate::tls::Method::DtlsServer,
-            #[cfg(boringssl)]
             ConnectionType::Datagram => crate::tls::Method::DtlsServerV1_3,
         };
 
         let cipher_list = match connection_type {
             ConnectionType::Stream => "TLS13-AES256-GCM-SHA384:TLS13-CHACHA20-POLY1305-SHA256",
-            ConnectionType::Datagram => {
-                "TLS13-CHACHA20-POLY1305-SHA256:ECDHE-RSA-CHACHA20-POLY1305:TLS13-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384"
-            }
+            ConnectionType::Datagram => "TLS13-CHACHA20-POLY1305-SHA256:TLS13-AES256-GCM-SHA384",
         };
 
         let tls_ctx = crate::tls::ContextBuilder::new(protocol)?
